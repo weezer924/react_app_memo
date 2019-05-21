78573afec5a53cf78a6d90116149422a91ae16e0
J.Cho <weezer970@gmail.com>
2019-05-21 09:26:35 +0900

    Setup All Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { memoReducer } from './memo/Store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const persistConfig = {
+  key: 'memo',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, memoReducer)
+
+let store = createStore(persistedReducer)
+let pstore = persistStore(store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
+
+export default pstore;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
